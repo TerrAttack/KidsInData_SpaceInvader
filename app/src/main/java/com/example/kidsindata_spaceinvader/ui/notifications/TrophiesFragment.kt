6229b6a3544f10296991179832f6af4b016b0d83
Zep Swijghuisen <zep.swijghuisen.reigersberg@hva.nl>
2021-03-13 12:58:21 +0100

    bottom navigation / beginning of data journey

--- app/src/main/java/com/example/kidsindata_spaceinvader/ui/notifications/TrophiesFragment.kt
+++ app/src/main/java/com/example/kidsindata_spaceinvader/ui/notifications/TrophiesFragment.kt
@@ -10,9 +10,9 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.kidsindata_spaceinvader.R
 
-class NotificationsFragment : Fragment() {
+class TrophiesFragment : Fragment() {
 
-    private lateinit var notificationsViewModel: NotificationsViewModel
+    private lateinit var notificationsViewModel: TrophiesViewModel
 
     override fun onCreateView(
             inflater: LayoutInflater,
@@ -20,8 +20,8 @@ class NotificationsFragment : Fragment() {
             savedInstanceState: Bundle?
     ): View? {
         notificationsViewModel =
-                ViewModelProvider(this).get(NotificationsViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_notifications, container, false)
+                ViewModelProvider(this).get(TrophiesViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_trophies, container, false)
         val textView: TextView = root.findViewById(R.id.text_notifications)
         notificationsViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
