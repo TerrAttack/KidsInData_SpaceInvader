07398f731a88e89a6588076e160efbe284d5b97e
Zep Swijghuisen <zep.swijghuisen.reigersberg@hva.nl>
2021-03-16 12:20:28 +0100

    data inladen RV

---
+++ app/src/main/java/com/example/kidsindata_spaceinvader/ui/data_journey/DataJourneyAdapter.kt
@@ -0,0 +1,53 @@
+package com.example.kidsindata_spaceinvader.ui.data_journey
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.kidsindata_spaceinvader.R
+import com.example.kidsindata_spaceinvader.databinding.ItemModuleBinding
+
+class DataJourneyAdapter(private val modules: List<Module>) : RecyclerView.Adapter<DataJourneyAdapter.ViewHolder>() {
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        val binding = ItemModuleBinding.bind(itemView)
+
+        fun dataBind(module: Module) {
+            binding.moduleNumber.text = module.moduleId.toString()
+            binding.moduleTitle.text = module.moduleName
+            binding.moduleDescription.text = module.moduleDescription
+
+            if (module.interactive == 1)
+                binding.interactiveStar.visibility = View.VISIBLE
+            else
+                binding.interactiveStar.visibility = View.GONE
+
+            if (module.moduleCompletedFlag) {
+                binding.completedFlag.setText(R.string.completed)
+                binding.completedFlag.setBackgroundResource(R.color.green)
+            } else {
+                binding.completedFlag.setText(R.string.notCompleted)
+                binding.completedFlag.setBackgroundResource(R.color.red)
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(
+            parent: ViewGroup,
+            viewType: Int
+    ): ViewHolder {
+        return ViewHolder(
+                LayoutInflater.from(parent.context).inflate(R.layout.item_module, parent, false)
+        )
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.dataBind(modules[position])
+    }
+
+    override fun getItemCount(): Int {
+        return modules.size
+    }
+
+}
--- app/src/main/java/com/example/kidsindata_spaceinvader/ui/data_journey/DataJourneyFragment.kt
+++ app/src/main/java/com/example/kidsindata_spaceinvader/ui/data_journey/DataJourneyFragment.kt
@@ -6,6 +6,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.kidsindata_spaceinvader.R
 import com.example.kidsindata_spaceinvader.databinding.FragmentDataJourneyBinding
 
@@ -14,6 +17,9 @@ class DataJourneyFragment : Fragment() {
 
     private var _binding: FragmentDataJourneyBinding? = null
     private val binding get() = _binding!!
+
+    private val modules = arrayListOf<Module>()
+    private val dataJourneyAdapter = DataJourneyAdapter(modules)
 
     private val AMOUNT_OF_MODULES = 5
     private var modulesCompleted = 0
@@ -30,12 +36,30 @@ class DataJourneyFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        initViews()
         updateProgress()
 
         binding.tenPlus.setOnClickListener {
             moduleCompleted()
             updateProgress()
         }
+    }
+
+    private fun initViews() {
+        binding.rvModules.layoutManager =
+                LinearLayoutManager(context, RecyclerView.VERTICAL, false)
+        binding.rvModules.adapter = dataJourneyAdapter
+
+        for (i in Module.MODULE_NAME.indices) {
+            modules.add(
+                    Module(i + 1,
+                            Module.MODULE_NAME[i],
+                            Module.MODULE_DESCRIPTION[i],
+                            Module.INTERACTIVE[i],
+                            Module.MODULE_COMPLETED_FLAG[i])
+            )
+        }
+        dataJourneyAdapter.notifyDataSetChanged()
     }
 
     private fun updateProgress() {
