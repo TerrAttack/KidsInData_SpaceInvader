49c01aab0ac3f934add2fc5a037eafa622dd98af
axelFV <axelvalent1998@gmail.com>
2021-03-31 14:54:15 +0200

    Changed homescreen
    Added buttons, navigation not complete yet

--- app/src/main/java/com/example/kidsindata_spaceinvader/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/kidsindata_spaceinvader/ui/home/HomeFragment.kt
@@ -4,36 +4,46 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.NavController
-import androidx.navigation.Navigation.findNavController
-import androidx.navigation.findNavController
+import androidx.navigation.Navigation
 import androidx.navigation.fragment.NavHostFragment
 import com.example.kidsindata_spaceinvader.R
-import com.example.kidsindata_spaceinvader.databinding.FragmentDataJourneyBinding
 import com.example.kidsindata_spaceinvader.databinding.FragmentHomeBinding
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
+
 
 class HomeFragment : Fragment() {
     private var _binding: FragmentHomeBinding? = null
-    private val binding get()= _binding!!
+    private val binding get() = _binding!!
 
-    private lateinit var homeViewModel: HomeViewModel
+    private lateinit var navController: NavController
 
     override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         return binding.root
     }
 
-    fun startGame(view: View) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+        binding.buttonGame.setOnClickListener {
+            navController.navigate(
+                R.id.action_fragment_home_screen_to_navigation_data_journey
+            )
+        }
+
+        binding.buttonDatajourney.setOnClickListener {
+            goToDatajourney()
+        }
+    }
+
+    private fun goToGame() {
+
+    }
+
+    private fun goToDatajourney() {
     }
 }
