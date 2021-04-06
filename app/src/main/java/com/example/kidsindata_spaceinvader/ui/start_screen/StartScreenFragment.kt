bc41733ed6488e5e957fafce0c8b6d2c6a0a9c2e
stalfah <floris.van.stal@hva.nl>
2021-04-06 23:36:10 +0200

    Added explanation screen and controls in dialog format

--- app/src/main/java/com/example/kidsindata_spaceinvader/ui/start_screen/StartScreenFragment.kt
+++ app/src/main/java/com/example/kidsindata_spaceinvader/ui/start_screen/StartScreenFragment.kt
@@ -4,14 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
-import androidx.navigation.findNavController
-import com.example.kidsindata_spaceinvader.R
 import com.example.kidsindata_spaceinvader.databinding.FragmentStartScreenBinding
-import com.example.kidsindata_spaceinvader.ui.dashboard.StartScreenViewModel
+import com.example.kidsindata_spaceinvader.ui.explanation.ExplanationDialogFragment
 
 class StartScreenFragment : Fragment() {
 
@@ -30,7 +25,8 @@ class StartScreenFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         binding.buttonNo.setOnClickListener {
-            view.findNavController().navigate(R.id.action_start_screen_to_explanationFirstFragment)
+            //Show the dialog fragment
+            ExplanationDialogFragment().show(parentFragmentManager, "custom Dialog")
         }
     }
 }
