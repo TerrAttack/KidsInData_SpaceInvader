14621ed687db1b9856010e0b9b1db5bac4f1ba75
axelFV <axelvalent1998@gmail.com>
2021-04-13 15:33:31 +0200

    HomeScreen stats
    De stats staan nu in de homescreen

--- app/src/main/java/com/example/kidsindata_spaceinvader/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/kidsindata_spaceinvader/ui/home/HomeFragment.kt
@@ -7,22 +7,24 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
+import androidx.fragment.app.activityViewModels
 import com.example.kidsindata_spaceinvader.DataJourneyActivity
-import com.example.kidsindata_spaceinvader.ui.data_journey.DataJourneyViewModel
+import com.example.kidsindata_spaceinvader.ui.trophies.TrophiesViewModel
 import com.example.numberskotlin.R
 import com.example.numberskotlin.databinding.FragmentHomeBinding
 
 
 class HomeFragment : Fragment() {
 
-    private val viewModel: DataJourneyViewModel by viewModels()
+    private val viewModelThrophy: TrophiesViewModel by activityViewModels()
+    private val viewModelHome: HomeViewModel by activityViewModels()
 
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
@@ -31,6 +33,11 @@ class HomeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        viewModelThrophy.getRank()
+        viewModelThrophy.getTopScore()
+        viewModelHome.getPlayerLatestScore()
+
         initViews()
     }
 
@@ -42,16 +49,36 @@ class HomeFragment : Fragment() {
             goToDataJourney()
         }
 
+        setHighScore()
+        setPlayerRanking()
+        setLastScore()
     }
 
     private fun goToGame() {
-
     }
 
     private fun goToDataJourney() {
         val intent = Intent(activity, DataJourneyActivity::class.java)
         startActivity(intent)
         activity?.overridePendingTransition(R.anim.slide_from_bottom, R.anim.slide_to_top)
+    }
+
+    private fun setHighScore() {
+        viewModelThrophy.trophiesTopScore.observe(viewLifecycleOwner, {
+            binding.tvHomeHighScore.text = it.toString()
+        })
+    }
+
+    private fun setPlayerRanking() {
+        viewModelThrophy.trophiesPlayerRank.observe(viewLifecycleOwner, {
+            binding.tvHomePlayerRanking.text = it.toString()
+        })
+    }
+
+    private fun setLastScore() {
+        viewModelHome.homePlayerLatestScore.observe(viewLifecycleOwner, {
+            binding.tvLastPlayed.text = it.playerScore.toString()
+        })
     }
 }
 
