c382215bbf1183d19a46c4b0121d52ec4a8eb13a
axelFV <axelvalent1998@gmail.com>
2021-05-18 16:04:50 +0200

    Viewmodel & Repository

---
+++ app/src/main/java/com/example/kidsindata_spaceinvader/repository/DashboardRepository.kt
@@ -0,0 +1,32 @@
+package com.example.kidsindata_spaceinvader.repository
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.example.kidsindata_spaceinvader.api.KidsInDataApi
+import com.example.kidsindata_spaceinvader.api.KidsInDataApiService
+import com.example.kidsindata_spaceinvader.model.TopScore
+import com.example.numberskotlin.BuildConfig
+import kotlinx.coroutines.withTimeout
+
+class DashboardRepository {
+    private val kidsInDataApiService: KidsInDataApiService = KidsInDataApi.createApi()
+
+    private val _dashboardTopScore: MutableLiveData<List<TopScore>> = MutableLiveData()
+
+    val dashboardTopScore: LiveData<List<TopScore>>
+        get() = _dashboardTopScore
+
+    suspend fun getLeaderboard(){
+        try{
+            val result = withTimeout(5_000) {
+                kidsInDataApiService.getTopTenScores(BuildConfig.ApiKey)
+            }
+
+            _dashboardTopScore.value = result
+        } catch (error: Throwable) {
+            throw HomeRepository.HomeRefreshError("Unable to refresh data", error)
+        }
+
+    }
+
+}
--- app/src/main/java/com/example/kidsindata_spaceinvader/vm/DashboardViewModel.kt
+++ app/src/main/java/com/example/kidsindata_spaceinvader/vm/DashboardViewModel.kt
@@ -1,13 +1,43 @@
 package com.example.kidsindata_spaceinvader.vm
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.kidsindata_spaceinvader.repository.DashboardRepository
+import com.example.kidsindata_spaceinvader.repository.DataJourneyRepository
+import kotlinx.coroutines.launch
+
 
 class DashboardViewModel : ViewModel() {
+    private val dashboardRepository = DashboardRepository()
+
+    private var _spinner: MutableLiveData<Boolean> = MutableLiveData()
+
+    private val _errorText: MutableLiveData<String> = MutableLiveData()
+
+    val spinner: LiveData<Boolean>
+        get() = _spinner
+
 
     private val _text = MutableLiveData<String>().apply {
         value = "This is dashboard Fragment"
     }
     val text: LiveData<String> = _text
+
+    fun getTopTenScores() {
+        viewModelScope.launch {
+            try {
+                _spinner.value = true
+                dashboardRepository.getLeaderboard()
+            } catch (error: DataJourneyRepository.DataJourneyRefreshError) {
+                _errorText.value = error.message
+                Log.e("Topscore error", error.cause.toString())
+            } finally {
+                _spinner.value = false
+            }
+        }
+    }
 }
+
